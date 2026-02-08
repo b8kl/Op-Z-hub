fdfe37daf72fe247446c934dac33d18d64b08b3d
b8kl <oklan9010@gmail.com>
2026-02-08 13:13:42 +0300

    Update Op z hub.lua

--- Op z hub.lua
+++ Op z hub.lua
@@ -11,20 +11,16 @@ local PlayerMouse = Player:GetMouse()
 local redzlib = {
 	Themes = {
 		Darker = {
-			["Color Hub 1"] = ColorSequence.new({
-	ColorSequenceKeypoint.new(0.00, Color3.fromRGB(8, 8, 8)),
-	ColorSequenceKeypoint.new(0.50, Color3.fromRGB(18, 18, 18)),
-	ColorSequenceKeypoint.new(1.00, Color3.fromRGB(8, 8, 8))
+["Color Hub 1"] = ColorSequence.new({
+ColorSequenceKeypoint.new(0.00, Color3.fromRGB(25, 25, 25)),
+ColorSequenceKeypoint.new(0.50, Color3.fromRGB(32.5, 32.5, 32.5)),
+ColorSequenceKeypoint.new(1.00, Color3.fromRGB(25, 25, 25))
 }),
-["Color Hub 2"] = Color3.fromRGB(12, 12, 12),
-["Color Stroke"] = ColorSequence.new({
-	ColorSequenceKeypoint.new(0.00, Color3.fromRGB(220, 220, 220)),
-	ColorSequenceKeypoint.new(0.50, Color3.fromRGB(160, 160, 160)),
-	ColorSequenceKeypoint.new(1.00, Color3.fromRGB(220, 220, 220))
-}),
-["Color Theme"] = Color3.fromRGB(200, 200, 200),
-["Color Text"] = Color3.fromRGB(245, 245, 245),
-["Color Dark Text"] = Color3.fromRGB(170, 170, 170)
+["Color Hub 2"] = Color3.fromRGB(30, 30, 30),
+["Color Stroke"] = Color3.fromRGB(40, 40, 40),
+["Color Theme"] = Color3.fromRGB(255, 255, 255),
+["Color Text"] = Color3.fromRGB(243, 243, 243),
+["Color Dark Text"] = Color3.fromRGB(180, 180, 180)
 },
 		Dark = {
 			["Color Hub 1"] = ColorSequence.new({
